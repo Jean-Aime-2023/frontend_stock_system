0fd1a67b849ba54bd61957a246f6ccf24b63a197
Jean-Aime-2023 <mugabejeanaime96@gmail.com>
2025-01-30 09:42:20 +0200

    changes

--- app/(dashboard)/reports/page.tsx
+++ app/(dashboard)/reports/page.tsx
@@ -1,8 +1,18 @@
+'use client'
+
 import Blank from '@/components/common/Blank';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const Reports = () => {
-  return <Blank text="No Reports available yet!" />;
+  const router = useRouter();
+  return (
+    <Blank
+      text="No Reports available yet!"
+      buttonText="Go back home"
+      onPress={() => router.push('/stock')}
+    />
+  );
 };
 
 export default Reports;
--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -1,8 +1,18 @@
+'use client';
+
 import Blank from '@/components/common/Blank';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const Settings = () => {
-  return <Blank text="Coming soon!" />;
+  const router = useRouter();
+  return (
+    <Blank
+      text="Coming soon!"
+      buttonText="Go back home"
+      onPress={() => router.push('/stock')}
+    />
+  );
 };
 
 export default Settings;
